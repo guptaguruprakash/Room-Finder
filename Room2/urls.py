d866b2437814c5987a1d4de9e96aaee4ad328bd8
Manoj Baniya <manojbaniya80@gmail.com>
2025-01-28 23:12:07 +0545

    body nav component added

--- Room2/urls.py
+++ Room2/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('login/',views.login,name='login'),
+    path('signup/',views.signup,name='signup'),
+    path('nav/',views.nav,name='nav'),
+    path('body/',views.body,name='body'),
     
     
  path("__reload__/", include("django_browser_reload.urls")),#removed after production
--- Room2/views.py
+++ Room2/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request,'index.html')
+
+def login(request): 
+  return render(request,'login.html')
+
+def signup(request):
+  return render(request,'signup.html')
+
+def nav(request):
+  return render(request,'nav.html')
+
+def body(request):
+  return render(request,'body.html')
